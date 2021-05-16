b496a206182328505a67e5a41c9e3cd3becdd995
ContronPAnda <rajtel99@gmail.com>
2021-05-15 23:33:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Discord LRU Sorter Bot
+
+This bot automatically sorts the channels and categories in your server based on how recently they've been used (had a message sent).
+More recently used channels go to the top of the category, and more recently used categories go to the top of the channel list.
+
+## Setup
+
+First, run `pip install -r requirements.txt` (you may need to change `pip` to something else like `pip3` depending on your system).
+Then, you can run the `config.py` script and follow the instructions to configure the bot.
+Finally, run `main.py` to actually run the bot.
---
+++ main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+from collections import defaultdict
+from datetime import datetime
+from operator import itemgetter
+
+try:
+    import discord
+    from discord.ext.commands import Bot
+    import dotenv
+except:
+    print(f"Please run `{sys.executable} pip install -r requirements.txt` to install requirements.", file=sys.stderr)
+    sys.exit(1)
+
+dotenv_file = dotenv.find_dotenv()
+config = {} if dotenv_file == '' else dotenv.dotenv_values(dotenv_file)
+
+if 'token' not in config or 'prefix' not in config:
+    print(f"Please run `config.py` to configure the bot.", file=sys.stderr)
+    sys.exit(1)
+
+bot = Bot(command_prefix=config['prefix'])
+
+def get_guild_list():
+    with open('.guilds', 'a+'): pass
+    with open('.guilds', 'r') as f:
+        return set(int(x) for x in f.readlines())
+
+def save_guild_list(guilds):
+    with open('.guilds', 'w+') as f:
+        for guild in guilds:
+            f.write(str(guild) + '\n')
+
+guilds = get_guild_list()
+print(f'Enabled Guilds: {guilds}')
+
+@bot.event
+async def on_ready():
+    print('Ready for action')
+
+@bot.command()
+async def enable(ctx):
+    """
+    Enable LRU on this server
+    """
+    if not ctx.guild:
+        await ctx.reply('This command doesn\'t work in DMs!')
+        return
+    print(f'Enabling {ctx.guild.id}')
+    if ctx.guild.id in guilds:
+        await ctx.reply('LRU is already enabled on this server!')
+    else:
+        await ctx.reply('Enabling LRU for this server')
+        guilds.add(ctx.guild.id)
+        save_guild_list(guilds)
+
+@bot.command()
+async def disable(ctx):
+    """
+    Disable LRU on this server
+    """
+    if not ctx.guild:
+        await ctx.reply('This command doesn\'t work in DMs!')
+        return
+    print(f'Disabling {ctx.guild.id}')
+    if ctx.guild.id in guilds:
+        await ctx.reply('Disabling LRU for this server')
+        guilds.remove(ctx.guild.id)
+        save_guild_list(guilds)
+    else:
+        await ctx.reply('LRU isn\'t enabled on this server!')
+
+@bot.command()
+async def sort(ctx):
+    """
+    Sort channels within categories and categories themselves in LRU order
+    """
+    if not ctx.guild:
+        await ctx.reply('This command doesn\'t work in DMs!')
+        return
+    print(f'Sorting {ctx.guild.id}')
+    await ctx.reply('Sorting channels...')
+
+    categories = defaultdict(lambda: [])
+
+    for channel in ctx.guild.text_channels:
+        last_msg = (await channel.fetch_message(channel.last_message_id)) if channel.last_message_id else None
+        categories[channel.category].append((channel, (last_msg.created_at if last_msg else datetime.fromtimestamp(0))))
+
+    for catlist in categories.values():
+        catlist.sort(key=itemgetter(1), reverse=True)
+        for i in range(len(catlist)):
+            await catlist[i][0].edit(position=i)
+
+    catsort = sorted([cat for cat in categories if cat], key=lambda cat: categories[cat][0][1], reverse=True)
+    for i in range(len(catsort)):
+        await catsort[i].edit(position=i)
+
+    await ctx.reply('Done sorting!')
+
+@bot.event
+async def on_message(message):
+    if message.guild.id in guilds:
+        await message.channel.edit(position=0)
+        if message.channel.category:
+            await message.channel.category.edit(position=0)
+
+    await bot.process_commands(message)
+
+bot.run(config['token'])
